0eb05c8197cd5f658682d0612f0d9a485417993b
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2020-12-26 23:17:22 -0500

    added demonstration for different routes for login service;

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TenantService } from '../tenant/tenant.service';
+import { Tenant, TenantService } from '../tenant/tenant.service';
 import { getLoginService, LoginService } from './login.service';
 import { Client1LoginService } from './client1login.service';
 import { Client2LoginService } from './client2login.service';
+import { Router, Routes } from '@angular/router';
 
 
 
@@ -18,4 +19,22 @@ import { Client2LoginService } from './client2login.service';
     }
   ]
 })
-export class LoginModule { }
+export class LoginModule {
+  constructor(private router: Router, private tenantService: TenantService) {
+    // configure the routes.
+    let appRoutes: Routes = [];
+
+    if (this.tenantService.getTenant() === Tenant.CLIENT1) {
+      console.log("Configuring CLIENT1 Login Components.");
+      appRoutes = [
+        // your routes here
+      ];
+    } else if (this.tenantService.getTenant() === Tenant.CLIENT2) {
+      console.log("Configuring CLIENT2 Login Components.");
+      appRoutes = [
+        // your routes here.
+      ];
+    }
+    appRoutes.forEach(e => this.router.config.unshift(e));
+  }
+}
